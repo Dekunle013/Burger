5843e820cfd9a3ffe9d100bafbaae4686d3942e0
Dekunle013 <Dekunle013@gmail.com>
2024-07-18 12:04:50 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Burger from './Burger'
+
+function App() {
+
+  return (
+    <>
+      <Burger />
+    </>
+  )
+}
+
+export default App
---
+++ src/Burger.jsx
@@ -0,0 +1,33 @@
+
+
+const Burger = () => {
+  return (
+    <>
+      <div className="parent">
+        <nav>
+          <section>
+            <img src="" alt="" />
+          </section>
+
+          <section>
+            <a href="#">Home</a>
+
+            <a href="#">About Us</a>
+
+            <a href="#">Services</a>
+            
+            <a href="#">Contact Us</a>
+          </section>
+
+          <section>
+            <a href="#">Login</a>
+
+            <a href="#">Sign Up</a>
+          </section>
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default Burger
