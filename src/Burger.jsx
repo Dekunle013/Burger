1b7e287c57db7fabe1e15b4cbc182716cf1fb55b
Dekunle013 <Dekunle013@gmail.com>
2024-07-18 12:16:08 +0100

    Work in progress

--- src/Burger.jsx
+++ src/Burger.jsx
@@ -6,7 +6,7 @@ const Burger = () => {
       <div className="parent">
         <nav>
           <section>
-            <img src="" alt="" />
+            <img src="https://s3-alpha-sig.figma.com/img/665b/73a1/e16849d1d67298e6541d04158fbf8703?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EBs95DAoJNBuqDvkjD5lKKBDDCmpZ7Djh5t2MTlVRgKEvIVL4cidjwXAmIvrkNiTiY603M0qmGOwB0EUKS3iv5OlNIAZ-spHBmMetxPZaWpdysD3IJV9pPBTxna-zdfb7qzvDErQasXGxM9JzhLdOK61-hX9Dm6qdAJ0bzm3Tci~sE35ll-Ycwm782JpWDUt74EvbxQoEfBYDQd8yIYXi9F6qzNVNJuKvBPEtHLUPU1v1eI0xjooX2QSf9ibuSR4vZiFu-3CCexhjIvZnU--k2gUBpX2yuMdeYfotbDsFmcEDPS24QhZIgEBvNViNBADqZDG4Wm8nB9z4D3sPk-PCw__" alt="Burger" />
           </section>
 
           <section>
